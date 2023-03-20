661b7e190aca133d9a92d3e8d1c76b7d7bb42bc4
aadin <aadin@DESKTOP-8OKL4OK>
2023-03-20 14:56:49 +1300

    Add project files.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int easyScore = 0;
+int mediumScore = 0;
+int hardScore = 0;
+
+Console.WriteLine("Welcome to the Māori Quiz!");
+Console.WriteLine("---------------------------");
+Console.WriteLine("Select a difficulty level: (e)asy, (m)edium, or (h)ard");
+string level = (Console.ReadLine()).ToUpper();
+
+if (level == "e")
+    Console.WriteLine("easy questions...");
+else if (level == "m")
+    Console.WriteLine("medium");
+else if (level == "h")
+    Console.WriteLine("hard questions");
+else Console.WriteLine("please enter a valid number");
