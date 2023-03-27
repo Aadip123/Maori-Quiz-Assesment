28f282e70e0164fdf8362e1af8b8fc57df53bd89
Aadi Panesar <ac121968@avcol.school.nz>
2023-03-28 12:49:04 +1300

    Added if statments for easy, working on hard

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -1,7 +1,14 @@
 ﻿
+using static System.Formats.Asn1.AsnWriter;
+using System.Numerics;
+using System.Reflection.Emit;
+
 int easyScore = 0;
 int mediumScore = 0;
 int hardScore = 0;
+
+int[] scores = new int[3];
+string[] levels = { "E", "M", "H" };
 
 Console.WriteLine("Welcome to the Māori Quiz!");
 Console.WriteLine("---------------------------");
@@ -10,68 +17,109 @@ string name = Console.ReadLine();
 Console.WriteLine($"Welcome {name}, we will start the quiz now");
 System.Threading.Thread.Sleep(1000);
 Console.Clear();
-Console.WriteLine("Select a difficulty level: (e)asy, (m)edium, or (h)ard");
-string level = Console.ReadLine().ToUpper();
-mediumScore=Medium(mediumScore, level);
-easyScore=Easy(easyScore, level);
+
+bool keepPlaying = true;
+
+while (keepPlaying)
+{
+    Console.WriteLine("Select a difficulty level: (E)asy, (M)edium, or (H)ard");
+    string level = Console.ReadLine().ToUpper();
+
+    if (level == "E")
+    {
+        easyScore = PlayQuiz("Easy", easyScore);
+    }
+    else if (level == "M")
+    {
+        mediumScore = PlayQuiz("Medium", mediumScore);
+    }
+    else if (level == "H")
+    {
+        hardScore = PlayQuiz("Hard", hardScore);
+    }
+    else
+    {
+        Console.WriteLine("Invalid level selected. Please try again.");
+        continue;
+    }
+
+    Console.WriteLine("Do you want to play again? (Y)es or (N)o");
+    string answer = Console.ReadLine().ToUpper();
+
+    if (answer == "N")
+    {
+        keepPlaying = false;
+    }
+
+    Console.WriteLine($"Thank you for playing! Your final scores are: Easy: {easyScore}, Medium: {mediumScore}, Hard: {hardScore}");
+    Console.ReadLine();
+
+}
+
+
+
+
+
 
 // method called easy showing score and level
-static int Easy(int easyScore, string level)
-{
-    if (level == "E")
-      // if statment checks what level is inputted
- 
+
+
+
+static int PlayQuiz(string levelName, int score)
     {
-        Console.WriteLine("Easy Questions:");
-        Console.WriteLine("----------------");
+        Console.WriteLine($"{levelName} Questions:");
+        Console.WriteLine("------------------");
 
         Console.WriteLine("1. What is the Māori word for hello?");
         Console.WriteLine("a) Kia ora");
         Console.WriteLine("b) Haere mai");
         Console.WriteLine("c) Ka kite");
         Console.WriteLine("d) Nau mai");
-        string easy1 = Console.ReadLine().ToUpper();
-        if (easy1 == "a")
+        string answer1 = Console.ReadLine().ToUpper();
+        if (answer1 == "A")
         {
-            easyScore++;
-            Console.WriteLine($"Correct point added your current score is {easyScore}");
-        }// adding score 
-        else if (easy1 != "a")
-        {
-            Console.WriteLine($"Sorry you have chosen an incorrect answer, your score is {easyScore}.");
+            score++;
+            Console.WriteLine($"Correct point added! Your current score is {score}");
         }
+        else
+        {
+            Console.WriteLine($"Sorry, you have chosen an incorrect answer. Your score is {score}.");
+        }
+
         Console.WriteLine("2. What is the Māori word for goodbye?");
         Console.WriteLine("a) E noho rā");
         Console.WriteLine("b) Kia ora");
         Console.WriteLine("c) Haere mai");
-        Console.WriteLine("d) Ma");
-        string easy2 = Console.ReadLine().ToUpper();
-        if (easy2 == "a")
+        Console.WriteLine("d) Mā");
+        string answer2 = Console.ReadLine().ToUpper();
+        if (answer2 == "A")
         {
-            easyScore++;
-            Console.WriteLine($"Correct point added! your current score is {easyScore}");
+            score++;
+            Console.WriteLine($"Correct point added! Your current score is {score}");
         }
-        else if (easy2 != "a")
+        else
         {
-            Console.WriteLine($"Sorry you have chosen an incorrect answer, your score is {easyScore}.");
+            Console.WriteLine($"Sorry, you have chosen an incorrect answer. Your score is {score}.");
         }
-       
-        // working on clicking key to medium level
 
+        Console.WriteLine($"Thank you for playing the {levelName} quiz. Your final score is {score}.");
+        return score;
     }
 
-    return easyScore;
-}
+
+
+
+
+
 
 //easy ends
-Console.WriteLine($"Thank you for playing your score is {easyScore} please click M to play Medium level or any other key to quit ");
-string nextlevel = Console.ReadLine().ToUpper();
 
 
 static int Medium(int mediumScore, string level)
 {// new level starts
     if (level == "M")
-        //if statment for medium level
+
+    //if statment for medium level
 
     {
         Console.WriteLine("Medium Questions:");
@@ -98,13 +146,13 @@ static int Medium(int mediumScore, string level)
         Console.WriteLine("b) Mana");
         Console.WriteLine("c) Tapu");
         Console.WriteLine("d) Mauri");
-        string medium2 = Console.ReadLine();
-        if (medium2 == "a")
+        string medium2 = Console.ReadLine().ToUpper();
+        if (medium2 == "A")
         {
             mediumScore++;
             Console.WriteLine($"Correct point added! your current score is {mediumScore}");
         }
-        else if (medium2 != "a")
+        else if (medium2 != "A")
         {
             Console.WriteLine($"Sorry you have chosen an incorrect answer, your score is {mediumScore}.");
         }
@@ -130,13 +178,13 @@ static int Hard(int hardScore, string level)
         Console.WriteLine("b) ");
         Console.WriteLine("c) ");
         Console.WriteLine("d) ");
-        string hard1 = Console.ReadLine();
-        if (hard1 == "a")
+        string hard1 = Console.ReadLine().ToUpper();
+        if (hard1 == "A")
         {
             hardScore++;
             Console.WriteLine($"Correct point added your current score is {hardScore}");
         }
-        else if (hard1 != "a")
+        else if (hard1 != "A")
         {
             Console.WriteLine($"Sorry you have chosen an incorrect answer, your score is {hardScore}.");
         }
@@ -145,13 +193,13 @@ static int Hard(int hardScore, string level)
         Console.WriteLine("b) Kia ora");
         Console.WriteLine("c) Haere mai");
         Console.WriteLine("d) Ma");
-        string hard2 = Console.ReadLine();
-        if (hard2 == "a")
+        string hard2 = Console.ReadLine().ToUpper();
+        if (hard2 == "A")
         {
             hardScore++;
             Console.WriteLine($"Correct point added! your current score is {hardScore}");
         }
-        else if (hard2 != "a")
+        else if (hard2 != "A")
         {
             Console.WriteLine($"Sorry you have chosen an incorrect answer, your score is {hardScore}.");
         }
